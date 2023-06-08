cc136618c36aaf5aa79c744f2c192bc9de951a05
diyorbek <diyorbekdk67@gmail.com>
2023-06-08 18:14:26 +0500

    homework/1/sal/o'xshamadi Fixes #1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
-<script setup>
-import { ref } from 'vue';
-
- const tasks= ref([]);
-
+<script>
+export default {
+  data() {
+    return {
+      inputText: "",
+      link: null
+    };
+  },
+  methods: {
+   clikmeLink() {
+      this.link = this.inputText;
+    },
+    deleteLink() {
+      this.link = null;
+    }
+  }
+};
 </script>
+
 <template>
-  <h1>
-    ToDo List
-  </h1>
-
-  <ol>
-    <li >
-      first task
-    </li>
-    <li>
-      second task
-    </li>
-  </ol>
-
-  <form action="">
-    <input type="text" name="task_name">
-    <button disabled>yaratish</button>
-  </form>
+  <p>To do Taks</p>
+   
+  <div>
+    <input class="input" type="text" v-model="inputText" />
+    <button class="btn" @click="clikmeLink">clik me</button>
+    <div v-if="link" class="link-container">
+      <button class="delet" @click="deleteLink">Delete</button>
+      <a  class="text" :href="link">{{ link }}</a>
+    </div>
+  </div>
 </template>
-<style scoped></style>
+
+
+<style>
+.text { 
+  color: black;
+  font-size: medium;
+ }
+.delet {
+background-color: red; 
+  color: white;
+  border-radius: 5px;
+}
+.input { 
+  border: 1px solid blueviolet;
+  border-radius: 10pxs;
+}
+.btn {
+  background-color: blueviolet;
+  color: white;
+  border: 1px solid black;
+  border-radius: 5px;
+}
+
+.link-container {
+  display: flex;
+  align-items: center;
+}
+
+.link-container button {
+  margin-right: 10px;
+}
+</style>
