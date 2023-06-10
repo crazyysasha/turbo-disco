1bd632883d2ff399c7b6f98cc649386102afff27
Otash1727 <q.s.otabek@gmail.com>
2023-06-10 19:51:07 +0500

    homework/1 lesson 41

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {ref} from 'vue'
+const first1=ref('');
+
+</script>
+
+
+
+<template>
+  <div>
+    <h1>{{first1}}</h1>
+    <input  v-model="first1">
+  </div>  
+</template>
