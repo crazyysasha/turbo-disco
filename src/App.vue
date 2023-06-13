5ee5bc4d43c1ea60671d45931678a03bb366f990
Otash1727 <128920121+Otash1727@users.noreply.github.com>
2023-06-13 17:45:42 +0300

    chala hali bitmagan

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,42 @@
 <script setup>
-import {ref} from 'vue'
-const first1=ref('');
+import { ref } from 'vue'
 
+const truefalse= ref(false);
+const hidden1=()=>{
+  if(!truefalse.value){
+    return 'hidden'
+    sss
+  }
+}
+const show1=()=>{
+  if(!truefalse.value){
+    return 'ism'
+  }
+}
+const ism=ref(['Rembo','Tini'])
 </script>
 
-
-
 <template>
-  <div>
-    <h1>{{first1}}</h1>
-    <input  v-model="first1">
-  </div>  
+  <header :class="hidden1">
+    <a href="">logo</a>
+  </header>
+  {{ truefalse}}
+ 
+  <button  v-on:click.stop="truefalse=!truefalse">
+    click me
+  </button>
+  <ul   :class="show1" v-if="ism.length>0">
+    <li v-for="ism1 in ism">
+         
+    </li>
+  </ul>
+  <p v-else-if="ism.length ==0">
+    royhat bo'sh
+  </p>
 </template>
+
+<style>
+.hidden{
+  display: none;
+}
+</style>
