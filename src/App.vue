dd5292f595f44b047b52ab3ede67c7b59143b067
Sasha Raimov <crazyysasha@gmail.com>
2023-06-13 18:50:17 +0500

    emit & props, built in  components

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,23 @@
 <script setup>
 import { ref } from 'vue';
+import CHeader from './components/CHeader.vue';
+import CLoginForm from './components/CLoginForm.vue'
 
+const isAuthed = ref(false);
 
-
-const isOpen = ref(false);
-
-const classes = () => {
-  if (!isOpen.value) {
-    return "hidden"
+const onLogin = (email, password) => {
+  if (email == 'admin@domain.com' && password == 'pass') {
+    isAuthed.value = true;
+  } else {
+    alert('parol yoki login xato!');
   }
-  return ''
 }
-
-const users = ref([
-  //  { name: 'Ravshan', age: 15 }, { name: 'Samir', age: 15 }, { name: 'Humoyin', age: 15 }
-]);
 </script>
 
 <template>
-  <header class="bg-gray-500" :class="classes()">
-    <a href="">logo</a>
-    <nav>
-      <a href="">menu 1</a>
-    </nav>
-  </header>
-  {{ isOpen }}
-  <button @:click.stop="isOpen = !isOpen">
-    menu
-  </button>
-  <ul v-if="users.length > 2">
-    <li v-for="{ name, age }, efsd in users">
-      {{ efsd + 1 }}. {{ name }} {{ age }}
-    </li>
-  </ul>
-  <p v-else-if="users.length > 0">
-    foydalanuvchilar 3 dan kam
-  </p>
-  <p v-else>
-    foydalanuvchilar yo'q
-  </p>
-
-  <input type="text" name="" id="" v>
+  <div class="flex flex-col min-h-screen bg-gray-100">
+    <CHeader :is-authed="isAuthed"></CHeader>
+    <CLoginForm v-if="!isAuthed" @on-submitted="onLogin"></CLoginForm>
+    <p class="text-center text-3xl text-green-700" v-else>welcome</p>
+  </div>
 </template>
-
-
-<style>
-.hidden {
-  display: none;
-}
-</style>
---
+++ src/components/CHeader.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+defineProps({
+    isAuthed: {
+        type: Boolean,
+        required: true,
+        default: false,
+    }
+});
+</script>
+<template>
+    <header class="container mx-auto px-5 py-4 flex justify-between">
+        <a href="">logo</a>
+        <nav class="flex gap-5">
+            <a href="">home</a>
+            <a href="" v-show="isAuthed">profile</a>
+        </nav>
+    </header>
+</template>
