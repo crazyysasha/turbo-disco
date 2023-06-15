38d8a0eb05e1b38bedda88338159100db3e654c5
Otash1727 <128920121+Otash1727@users.noreply.github.com>
2023-06-15 07:16:33 +0300

    fix new

---
+++ src/components/Rg.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref} from 'vue';
+defineProps({
+    isAuthed:{
+        type: Boolean,
+        required: true,
+        default: false
+    }}
+)
+
+
+</script>
+
+<template>
+    <header class="container mx-auto  flex justify-between">
+        <a href="">logo</a>
+        <a href="">home</a>
+        <button class="bg-green-300 rounded-3xl text-ellipsis hover:bg-teal-400" v-show="isAuthed" v-on:click="isAuthed=!isAuthed"> LogOut</button>
+    </header>
+</template>
