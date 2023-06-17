630dfdf79d2e87eaf7bf14702e3d9166e285cdb3
Sasha Raimov <crazyysasha@gmail.com>
2023-06-17 18:31:17 +0500

    chat first task complete Fixes #6

---
+++ src/views/ChatView.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+<template>
+    <main class="flex">
+        <div>
+            <!-- users list component -->
+        </div>
+        <div>
+            <!-- chat component -->
+        </div>
+        <div>
+            <!-- profile component -->
+        </div>
+    </main>
+</template>
