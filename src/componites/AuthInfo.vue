4250f947f84ea9aecfd2f2e08a7cbbdc829f7e91
Otash1727 <128920121+Otash1727@users.noreply.github.com>
2023-06-22 16:44:04 +0300

    new new new

--- src/componites/AuthInfo.vue
+++ src/componites/AuthInfo.vue
@@ -1,10 +1,28 @@
 <script setup>
+defineProps({
+    Users:{
+        type:String,
+        required:true,
+        default:'Foydalanuvchi ismi'
+    },
+    Pass:{
+        type:String,
+        required:true,
+        default:'Parol'
+    },
+    Passrecovery:{
+        type:String,
+        required:true,
+        default:'Parolni tiklash'
+    },
+})
 </script>
 <template>
     <div class=" flex flex-col items-center">
-        <label for="" class="text-[#ABAAAA] font-bold f"> User name </label>
+        <label for="" class="text-[#ABAAAA] font-bold f"> {{ Users }} </label>
         <input type="text" class="rounded-2xl border-solid border-2 border-gray-500">
-        <label for="" class="text-[#ABAAAA] font-bold"> Password</label>
+        <label for="" class="text-[#ABAAAA] font-bold">{{ Pass }}</label>
         <input type="text" class="rounded-2xl border-solid border-2 border-gray-500">
+        <a href="" class="text-[#301B52] font-bold"> {{ Passrecovery }} </a>
     </div>
 </template>
