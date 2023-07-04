558c068593af0153e108f100582b1b30e2bd657f
Sasha Raimov <crazyysasha@gmail.com>
2023-07-04 18:15:42 +0500

    many fixes

--- src/stores/homework.js
+++ src/stores/homework.js
@@ -1,55 +1,54 @@
-import {defineStore} from 'pinia';
-import {ref} from 'vue';
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
 
-export const DohomeworkStore=defineStore('homework',()=>{
-    const creatid=ref(3)
-    const answer=ref([
+export const DohomeworkStore = defineStore('homework', () => {
+    const creatid = ref(3)
+    const answer = ref([
         {
-            id:1,
-            title:' first question',
-            isCompleted:false,
+            id: 1,
+            title: ' first question',
+            isCompleted: false,
         },
         {
-            id:2,
+            id: 2,
             title: 'Second question',
-            isCompleted:false,
+            isCompleted: false,
 
         }
     ]);
-    function toggle(id){
-        answer.value=answer.value.map((newasn)=>{
-            if(id==newasn.id){
-                newasn.isCompleted=!newasn.isCompleted;
+    function toggle(id) {
+        answer.value = answer.value.map((newasn) => {
+            if (id == newasn.id) {
+                newasn.isCompleted = !newasn.isCompleted;
             }
             return newasn;
         })
-    }    
-
-    function create(title){
-       answer.value.push({
-        id:creatid.value++,
-        title:title,
-        isCompleted:false,
-       })
     }
-    function remove(title,id){
+
+    function create(title) {
+        answer.value.push({
+            id: creatid.value++,
+            title: title,
+            isCompleted: false,
+        })
+    }
+    function remove(title, id) {
         answer.value.pop({
             id,
             title,
-            isCompleted:false
+            isCompleted: false
         })
     }
-    function update(title){
-        answer.value.splice({
-            id:this.id,
-            title,
-            isCompleted:false
-        })
+    function update(id, title) {
+        answer.value = answer.value.map((todo) => {
+            if (todo.id == id) todo.title = title;
+            return todo;
+        });
     }
 
-    
-    
 
-    return{answer,create,toggle,remove,update}
+
+
+    return { answer, create, toggle, remove, update }
 })
--- src/views/HomeworkView.vue
+++ src/views/HomeworkView.vue
@@ -1,46 +1,62 @@
 <script setup>
-import {DohomeworkStore} from '../stores/homework';
-import {ref,reactive} from 'vue';
+import { DohomeworkStore } from '../stores/homework';
+import { ref } from 'vue';
 import FormInfo from '../componites/Mix/FormInfo.vue';
-const created=ref('')
-const store=DohomeworkStore();
-const infoform=reactive({
-    Rgform:'Registration form',
-    name:'Name',
-    pass:'Password',
-    email:'Email',
-    mobile:'Phone',
-    gender:'Gender',
-    customer:'Are you an exsiting customer?'
-})
+const created = ref('')
+const title = ref('');
+const store = DohomeworkStore();
+const infoform = {
+    Rgform: 'Registration form',
+    name: 'Name',
+    pass: 'Password',
+    email: 'Email',
+    mobile: 'Phone',
+    gender: 'Gender',
+    customer: 'Are you an exsiting customer?'
+};
+
+const updateId = ref(null);
+const updateTitle = ref('');
+
 </script>
 
 <template>
     <div class=" bg-amber-400 min-h-screen flex mx-auto ">
-        <!-- <div> -->
-        <!-- <ul class=" mx-4 py-5 flex flex-col gap-6"> -->
-            <!-- <li v-for="home, index in store.answer" class="text-2xl font-Roboto text-purple-700"> -->
-                <!-- {{ index+1 }}.{{ home.title }} -->
-                <!-- <button class="rounded-2xl bg-gray-400 hover:bg-red-500 hover:text-black transation-opacity mx-4 px-4 py-2 text-2xl"  -->
-                <!-- @click="store.remove(title,id)">  -->
-                    <!-- delete -->
-                <!-- </button> -->
-                <!-- <button  class="rounded-2xl bg-gray-400 hover:bg-lime-500 mx-4 px-4 py-2 text-2xl"> -->
-                    <!-- update -->
-                <!-- </button>               -->
-            <!-- </li> -->
-        <!-- </ul> -->
-        <!-- <form @submit.prevent="store.create(title); title=''"> -->
-            <!-- <input type="text" placeholder="Write your answers" class="py-2 px-4 mx-4 rounded-3xl font-Roboto text-purple-700 " v-model="title"> -->
-            <!-- <button class="rounded-2xl bg-gray-400 hover:bg-lime-500 mx-4 px-4 py-2 text-2xl"> Send </button>            -->
-        <!-- </form> -->
-        <!-- </div>       -->
-        <!-- <div>   -->
-<!--  -->
-        <!-- </div> -->
-    <!--  -->
-            <!--  -->
-            <FormInfo :="infoform"></FormInfo>
+        <div>
+            <ul class=" mx-4 py-5 flex flex-col gap-6">
+                <li v-for="home, index in store.answer" class="text-2xl font-Roboto text-purple-700">
+                    <div v-if="home.id != updateId">
+                        {{ index + 1 }}.{{ home.title }}
+                        <button
+                            class="rounded-2xl bg-gray-400 hover:bg-red-500 hover:text-black transation-opacity mx-4 px-4 py-2 text-2xl"
+                            @click="store.remove(title, id)">
+                            delete
+                        </button>
+                        <button class="rounded-2xl bg-gray-400 hover:bg-lime-500 mx-4 px-4 py-2 text-2xl"
+                            @click="updateId = home.id; updateTitle = home.title">
+                            update
+                        </button>
+                    </div>
+                    <div v-else>
+
+                        <textarea v-model="updateTitle"></textarea>
+                        <button @click="store.update(updateId, updateTitle); updateId = null">
+                            save
+                        </button>
+                    </div>
+                </li>
+            </ul>
+            <form @submit.prevent="store.create(title); title = ''">
+                <input type="text" placeholder="Write your answers"
+                    class="py-2 px-4 mx-4 rounded-3xl font-Roboto text-purple-700 " v-model="title">
+                <button class="rounded-2xl bg-gray-400 hover:bg-lime-500 mx-4 px-4 py-2 text-2xl"> Send </button>
+            </form>
+        </div>
+        <div>
+
+        </div>
+
+
+        <FormInfo :="infoform"></FormInfo>
     </div>
-    
 </template>
